964ec03a87219f35ba6bd031f234c07cadf2350e
Drew <hopkind4@tcnj.edu>
2019-12-05 20:52:25 -0500

    checks for duplicate file names

--- src/fileCreeper.py
+++ src/fileCreeper.py
@@ -7,19 +7,29 @@ path = root + r"/Collected Items"
 os.mkdir(path)
 
 folder = "level"
-path = root
 counter = 0
 
 arr = (".csv",".jpg",".doc",".png",".txt",".pdf",".docx",".mp3",".ai",".psd",".py",".ppt",".pps",".pptx",".xls",".xlsx",".c")
 
 
-for (root,dirs,files) in os.walk(path, topdown=True): 
+for (root,dirs,files) in os.walk(root, topdown=True): 
     #print (root)
-    for fileName in files: # for each file in the directory
-        folderPath = path + r"/"+ fileName[fileName.find(".")+1:]
-        folderExists = os.path.isdir(folderPath)
-        if (fileName.endswith(arr))and(folderExists == False): #if the file ends in the correct extension
-            os.mkdir(folderPath)
-            shutil.move(root+ r"/"+ fileName,folderPath)
-        elif(fileName.endswith(arr)):
-            shutil.move(root+ r"/"+ fileName,folderPath)
+    rootCotainsPath = root.find(path)
+    if(rootCotainsPath):
+        for fileName in files: # for each file in the directory
+            folderPath = path + r"/"+ fileName[fileName.find(".")+1:]
+            folderExists = os.path.isdir(folderPath)
+            if (fileName.endswith(arr))and(folderExists == False): #if the file ends in the correct extension
+                os.mkdir(folderPath)
+                shutil.move(root+ r"/"+ fileName,folderPath)
+            elif(fileName.endswith(arr)):
+                if(os.path.exists(folderPath+ r"/"+ fileName)): # if the file exists
+                    tempName = fileName
+                    num = 0
+                    while(os.path.exists(folderPath+ r"/"+ tempName)):
+                        tempName = fileName[:fileName.find(".")] + "("+str(num)+")" + fileName[fileName.find("."):]
+                        print (tempName)
+                        num += 1
+                    shutil.move(root+ r"/"+ fileName,folderPath+ r"/"+tempName)
+                else:
+                    shutil.move(root+ r"/"+ fileName,folderPath)
